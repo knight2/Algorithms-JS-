059ae05b33a7d431e13b185dcb3617997c338166
Knight2 <8440420+knight2@users.noreply.github.com>
2020-04-02 09:13:41 -0400

    symmetric difference function for two arrays

--- symmetric difference.js
+++ symmetric difference.js
@@ -1,3 +1,25 @@
-var message ="hello world";
+//Create a function that takes two or more arrays and returns
+// an array of the syemmtric difference of the provided arrays
 
-console.log(message); 
+function symmetricDifference(arrayOne, arrayTwo){
+    var arrDifference = [];
+
+    arrayOne.forEach(function(item){
+        if (arrayTwo.indexOf(item) < 0 && arrDifference.indexOf(item) <0){
+            arrDifference.push(item);
+        } 
+    });
+
+    arrayTwo.forEach(function(item){
+        if (arrayOne.indexOf(item) < 0 && arrDifference.indexOf(item) <0){
+            arrDifference.push(item);
+        }
+    });
+
+    return arrDifference;
+}
+
+var arr1 = [1,2,3];
+var arr2 = [2,2,5];
+
+console.log(symmetricDifference(arr1, arr2));
