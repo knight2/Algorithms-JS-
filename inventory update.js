98473fdf3e106f68601eb2db54e5d94bafe92115
Knight2 <8440420+knight2@users.noreply.github.com>
2020-04-04 21:05:29 -0400

    inventory update algo

---
+++ inventory update.js
@@ -0,0 +1,10 @@
+//Compare and update the inventory stored in a 2D array against a second 2D array of
+// a fresh delivery.
+// Update the current existing inventory item quantities (in arr1). IF an item 
+//cannot be found, add the new item and quantity into the inventory array.
+//The returned inventory array should be in alphabetical order.
+
+
+function updateInventory(arr1, arr2){
+    
+}
