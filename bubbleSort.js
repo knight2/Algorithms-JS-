1fffe1930881105950efc072e2054b399ad8cdbf
Knight2 <8440420+knight2@users.noreply.github.com>
2020-04-10 12:15:23 -0400

    Bubble sort initial commit

---
+++ bubbleSort.js
@@ -0,0 +1,22 @@
+/*
+Bubble Sort Method
+Starts at the beginning of an unsorted array
+and 'bubbles up' unsorted values towards
+the end, iterating through the array
+until it is completely sorted.
+It does this by comparing adjacent items and swapping
+them if they are out of order.
+The method continues looping through the array
+until no swaps occur. at which point the array
+is sorted.
+
+Requires multiple iterations and has quadratic time complexity
+*/
+
+function bubbleSort(array){
+
+
+    return array;
+}
+
+console.log(bubbleSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]));
