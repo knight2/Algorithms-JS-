060de819af1d0d666c6d6d475697b73b556268e5
Knight2 <8440420+knight2@users.noreply.github.com>
2020-04-11 14:56:59 -0400

    selection sort initial commit

---
+++ selectionSort.js
@@ -0,0 +1,23 @@
+/*
+Selection Sort
+
+We select the minimum value in a list and 
+swap it with the first value in the list.
+Then it starts at the second position,
+selecting the smallest value in the
+remaining list. It continues
+iterating through the list and swapping
+elements until it reaches the end of the
+list. Now the list is sorted.
+Selection sort has quadratic time complexity
+in all cases.
+*/
+
+
+function selectionSort(array){
+
+
+    return array;
+}
+
+console.log(selectionSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]));
