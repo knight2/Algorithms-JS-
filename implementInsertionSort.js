dd2edd3bef6f66fcb8129caebc726d694ddc27f7
Knight2 <8440420+knight2@users.noreply.github.com>
2020-04-12 10:51:47 -0400

    function insertionSort solution

--- implementInsertionSort.js
+++ implementInsertionSort.js
@@ -8,8 +8,14 @@ in the average and worse cases.
 */
 
 function insertionSort(array){
-
+    for (let i = 1; i < array.length; i++){
+        let current = array[i];
+        for (var j = i -1; j>=0 && array[j] > current; j--){
+            array[j + 1] = array[j];
+        }
+        array [j+1] = current;
+    }
     return array;
 }
 
-insertionSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]);
+console.log(insertionSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]));
