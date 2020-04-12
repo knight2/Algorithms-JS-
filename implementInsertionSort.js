1be9cd10ba00bd953edcd3e4cb083f68d8e0ba8b
Knight2 <8440420+knight2@users.noreply.github.com>
2020-04-12 10:41:42 -0400

    insertion sort initial commit

---
+++ implementInsertionSort.js
@@ -0,0 +1,15 @@
+/*
+This sorting method works by building up a sorted array at the beginning of a list.
+It begins the sorted array with the first element. Then it inspects the next
+element and swaps it backwards into the sorted array until it is in sorted position.
+It continues iterating through the list and swapping new items backwards into the sorted
+portion until it reaches the end. This algorithm has quadratic time complexity
+in the average and worse cases.
+*/
+
+function insertionSort(array){
+
+    return array;
+}
+
+insertionSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]);
